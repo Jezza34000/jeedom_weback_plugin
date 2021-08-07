8ca37894129b046dc7b80cfaf27b4f6e7e863656
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2021-08-07 22:56:42 +0200

    Update weback.ajax.php

--- core/ajax/weback.ajax.php
+++ core/ajax/weback.ajax.php
@@ -33,6 +33,7 @@ try {
     if (init('action') == 'postSave') {
       weback::getToken();
       weback::getAWScredential();
+      weback::getDeviceList()
       //mitsubishi::refreshAll();
       ajax::success();
     }
