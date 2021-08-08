a6f10109ec91f4b8b4a662fd6c34bfd409214491
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2021-08-08 09:06:17 +0200

    Update weback.class.php

--- core/class/weback.class.php
+++ core/class/weback.class.php
@@ -133,7 +133,7 @@ class weback extends eqLogic {
            config::save("AccessKeyId", $json['Credentials']['AccessKeyId'], 'weback');
            config::save("Expiration", $json['Credentials']['Expiration'], 'weback');
            config::save("SecretKey", $json['Credentials']['SecretKey'], 'weback');
-           config::save("IdentityId", $json['Credentials']['IdentityId'], 'weback');
+           config::save("IdentityId", $json['IdentityId'], 'weback');
            config::save("SessionToken", $json['Credentials']['SessionToken'], 'weback');
          } else {
            log::add('weback', 'debug', 'Erreur CURL = ' . curl_error($ch));
