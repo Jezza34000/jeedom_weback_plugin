a35b79c6b5702a91586e0616110733fb53d6e0f0
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2021-08-08 22:23:46 +0200

    Update weback.class.php

--- core/class/weback.class.php
+++ core/class/weback.class.php
@@ -204,7 +204,7 @@ class weback extends eqLogic {
              $robot->setLogicalId($json['Request_Cotent'][0]['Thing_Nick_Name'].$json['Request_Cotent'][0]['Thing_Name']);
              $robot->setIsEnable(1);
              $robot->setIsVisible(1);
-             $robot->setName('MonRobot '.$json['Request_Cotent'][0]['Sub_type']);
+             $robot->setName($json['Request_Cotent'][0]['Thing_Name']." ".$json['Request_Cotent'][0]['Sub_type']);
              $robot->setConfiguration('Thing_Nick_Name', $json['Request_Cotent'][0]['Thing_Nick_Name']);
              $robot->setConfiguration('Sub_type', $json['Request_Cotent'][0]['Sub_type']);
              $robot->setConfiguration('Thing_Name', $json['Request_Cotent'][0]['Thing_Name']);
