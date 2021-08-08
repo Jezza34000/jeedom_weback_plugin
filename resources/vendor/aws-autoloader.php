68b1f75a868c860572d054dfaf1460246091b59b
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2021-08-08 07:18:57 +0200

    Update aws-autoloader.php

--- resources/vendor/aws-autoloader.php
+++ resources/vendor/aws-autoloader.php
@@ -1691,7 +1691,7 @@ spl_autoload_register(function ($class) use ($mapping) {
     }
 }, true);
 
-//require __DIR__ . '/Aws/functions.php';
+require __DIR__ . '/Aws/functions.php';
 require __DIR__ . '/GuzzleHttp/functions_include.php';
 require __DIR__ . '/GuzzleHttp/Psr7/functions_include.php';
 require __DIR__ . '/GuzzleHttp/Promise/functions_include.php';
