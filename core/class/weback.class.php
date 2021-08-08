e0ce548aa2460c24c06eb06503399ff4c9e9cc97
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2021-08-08 07:11:43 +0200

    1

--- core/class/weback.class.php
+++ core/class/weback.class.php
@@ -20,6 +20,7 @@
 
 /* * ***************************Includes********************************* */
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
+require_once __DIR__ . '/../../resources/moduletest.php';
 //require_once __DIR__ . '/ressources/node_modules/aws-autoloader.php';
 
 //use Aws\Lambda\LambdaClient;
