bbcf7478f91ad44b6e72bced9d9dccbab618f398
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2021-08-09 13:19:22 +0200

    Update weback.class.php

--- core/class/weback.class.php
+++ core/class/weback.class.php
@@ -251,7 +251,8 @@ class weback extends eqLogic {
       log::add('weback', 'debug', 'IOT Return : ' . $return);
       $shadowJson = json_decode($return, false);
       log::add('weback', 'debug', 'Mise à jours des INFO (Status='.$shadowJson->state->reported->working_status.')');
-      $weback->checkAndUpdateCmd('working_status', $shadowJson->state->reported->working_status);
+      //$weback->checkAndUpdateCmd('working_status', $shadowJson->state->reported->working_status);
+      $weback->checkAndUpdateCmd('working_status', 'test');
     }
 
     public static function IsRenewlRequired(){
