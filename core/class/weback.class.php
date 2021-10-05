e82dc407fd3056dc0cb8f6263f50a3abe36807e7
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2021-10-05 09:16:55 +0200

    Update weback.class.php

--- core/class/weback.class.php
+++ core/class/weback.class.php
@@ -462,7 +462,7 @@ class weback extends eqLogic {
         return;
       }
       $content = file_get_contents(dirname(__FILE__) . '/../config/devices/' . $_type . '.json');
-      log::add('weback', 'error', 'Content : '.$content);
+      //log::add('weback', 'error', 'Content : '.$content);
       if (!is_json($content)) {
         log::add('weback', 'error', 'Format du fichier de configuration n\'est pas du JSON valide !');
         return;
